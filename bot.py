21af58abed4024ddac347d0be0860dcac8e71325
Radwayne12 <157649019+Radwayne12@users.noreply.github.com>
2024-05-13 00:00:26 +0200

    setup webdriver

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.select import Select
+from time import sleep
 
+# Fix auto closing browser
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+
+driver.get('http://127.0.0.1:5000')
